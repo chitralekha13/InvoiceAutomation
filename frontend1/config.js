781c8130677f4f45bdbdb92e433aabd17a83c0ba
Chitra Sura <68381259+chitralekha13@users.noreply.github.com>
2026-02-16 17:42:08 -0800

    Update API base URL to use HTTP

--- frontend1/config.js
+++ frontend1/config.js
@@ -2,8 +2,9 @@
 // For local dev: copy config.local.js.example to config.local.js and set values
 // For production: pipeline overwrites this with secrets (AZURE_CLIENT_ID, etc.)
 window.APP_CONFIG = {
-    apiBaseUrl: "invoiceautomation-bdcudzfpe9cpf4d5.westus2-01.azurewebsites.net",
+    apiBaseUrl: "http://invoiceautomation-bdcudzfpe9cpf4d5.westus2-01.azurewebsites.net/",
     clientId: "",
     redirectUri: window.location.origin + "/index.html"
 };
+
 
