0cca81eecb54564284e9e258f7f569b330c09188
poorrnimavetrivelan <poorrnimavetrivelan@gmail.com>
2026-02-25 16:24:55 -0800

    Fix Client ID - 1

--- accountsdashboard/config.js
+++ accountsdashboard/config.js
@@ -2,6 +2,6 @@
 // Pipeline overwrites this on deploy. For local dev: copy config.local.js.example to config.local.js
 window.APP_CONFIG = {
     apiBaseUrl: "https://invoiceautomation-bdcudzfpe9cpf4d5.westus2-01.azurewebsites.net",
-    clientId: "",
+    clientId: "4b952eaf-5b8e-4ddc-a096-561c0abe771f",
     redirectUri: window.location.origin + "/index.html"
 };
